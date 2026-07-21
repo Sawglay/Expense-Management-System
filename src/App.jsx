543d3900bfb163a8c9913442bff4bc3b689b0c95
Sawglay <sawglay2@gmail.com>
2026-07-22 01:07:10 +0700

    Added React Component and header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>
+          Expense Management
+        </h1>
+      </header>
+    </div>
+  )
+}
