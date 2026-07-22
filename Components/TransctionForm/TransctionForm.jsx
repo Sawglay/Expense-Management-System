067e7856350173f7675534b3e9fa6f8eececc082
Sawglay <sawglay2@gmail.com>
2026-07-23 00:45:16 +0700

    Updated Css and added TransctionForm folder including jsx and css

---
+++ Components/TransctionForm/TransctionForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TransctionForm.css'
+
+const TransctionForm = () => {
+  return (
+    <div>TransctionForm</div>
+  )
+}
+
+export default TransctionForm
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './App.css';
+import SummaryCards from '../Components/SummaryCards/summarycards';
+
 const App = () => {
   return (
     <div>
       <header>
         <h1>
-          Expense Management
+          Expense Management System
         </h1>
+
+        <SummaryCards/>
+
+        <div className='dashboard'>
+          <div className='left panel'>
+
+          </div>
+        </div>
       </header>
     </div>
   )
