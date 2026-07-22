bd5369fe08832cffb7d064ed21dda32a0df12e8b
Sawglay <sawglay2@gmail.com>
2026-07-23 00:08:39 +0700

    Added and Updated Card css

--- Components/SummaryCards/SummaryCards.jsx
+++ Components/SummaryCards/SummaryCards.jsx
@@ -6,17 +6,17 @@ const SummaryCards = () => {
     <div className='summary-cards'>
       <div className='summary-card'>
         <h3>Hello Balance</h3>
-        <p className='amount'>1000</p>
+        <p className='amount balance'>1000</p>
       </div>
 
       <div className='summary-card'>
         <h3>Income</h3>
-        <p className='amount'>1000</p>
+        <p className='amount income'>1000</p>
       </div>
 
       <div className='summary-card'>
         <h3>Expense</h3>
-        <p className='amount'>1000</p>
+        <p className='amount expense'>1000</p>
       </div>  
     </div>
   )
